c5a5492f6bd09f2cacd647226eaf6c17ca9db219
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-09-20 04:11:50 +0000

    feat(api): implement edge runtime

--- src/app/api/exit-preview/route.ts
+++ src/app/api/exit-preview/route.ts
@@ -2,6 +2,8 @@ import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET() {
   draftMode().disable();
   redirect(`/`);
