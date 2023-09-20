a199aa435e83706a1be8f7c8d97a4dc393035bbd
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-09-20 04:07:50 +0000

    fix(api): return response

--- src/app/api/exit-preview/route.ts
+++ src/app/api/exit-preview/route.ts
@@ -1,7 +1,9 @@
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   draftMode().disable();
   redirect(`/`);
+  return NextResponse.json("Draft mode disabled.");
 }
--- src/app/api/preview/route.ts
+++ src/app/api/preview/route.ts
@@ -1,7 +1,9 @@
 import { draftMode } from "next/headers";
 import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   draftMode().enable();
   redirect(`/`);
+  return NextResponse.json("Draft mode enabled.");
 }
