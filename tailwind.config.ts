f51d7979345c24aee96e331f06cf89739f7c6835
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2023-10-30 10:55:20 +0700

    feat(tw): use TS for config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
@@ -9,4 +10,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+} as Config;
