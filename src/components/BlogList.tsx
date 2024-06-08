8038c82642f3e1f19d13f7b7423e4a39a588e5b7
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-06-08 05:56:16 +0000

    fix(app): remove client side route

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,25 +1,20 @@
-import React from "react";
+import Link from "next/link";
+
 import BlogItem from "./BlogItem";
-import ClientSideRoute from "./ClientSideRoute";
 
-type Props = {
-  posts: Post[];
-};
-
-const BlogList = ({ posts }: Props) => {
+function BlogList({ posts }: { posts: Post[] }) {
   return (
     <div>
       <hr className="border-[#F7AB0A] mb-10" />
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
-        {/* Posts */}
         {posts.map((post) => (
-          <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
             <BlogItem post={post} />
-          </ClientSideRoute>
+          </Link>
         ))}
       </div>
     </div>
   );
-};
+}
 
 export default BlogList;
