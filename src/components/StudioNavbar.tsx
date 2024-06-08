91e68b4c6869031dc000755c4f3d80b48482d836
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-06-08 07:31:20 +0000

    feat(studio): banner facelift

--- src/components/StudioNavbar.tsx
+++ src/components/StudioNavbar.tsx
@@ -5,18 +5,15 @@ import { ArrowUturnLeftIcon } from "@heroicons/react/24/solid";
 const StudioNavbar = (props: any) => {
   return (
     <div>
-      <div className="flex items-center justify-between p-5">
-        <Link className="text-[#F7AB0A] flex items-center" href="/">
+      <div className="flex items-center justify-between p-4 pb-2">
+        <Link className="text-[#F7AB0A] flex items-center hover:underline" href="/">
           <ArrowUturnLeftIcon className="h-5 w-5 mr-2 text-[#F7AB0A]" />
           Go To Website
         </Link>
 
-        <div className="hidden md:flex px-5 py-3 rounded-lg justify-center border-2 border-[#F7AB0A]">
-          <h1 className="font-bold text-white">
-            Welcome to The Blog CMS! Brought to you by
-          </h1>
+        <div className="hidden md:flex px-3 py-1.5 rounded-md justify-center border-[1px] border-[#F7AB0A]">
           <Link
-            className="text-[#F7AB0A] font-bold ml-2"
+            className="text-[#F7AB0A] font-bold"
             href="https://www.sanity.io"
           >
             www.sanity.io
