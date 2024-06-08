635cb34c322a1c49cd0dc5fe7a0cd743e057db1f
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-06-08 06:27:53 +0000

    fix(imports): use alias

--- src/sanity.config.ts
+++ src/sanity.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from "sanity";
 import { presentationTool } from 'sanity/presentation';
 import { structureTool } from "sanity/structure";
 
-import Logo from "./components/Logo";
-import StudioNavbar from "./components/StudioNavbar";
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schema";
-import { defaultDocumentNode } from "./sanity/structure";
-import { theme } from "./sanity/theme";
+import Logo from "@/components/Logo";
+import StudioNavbar from "@/components/StudioNavbar";
+import { apiVersion, dataset, projectId } from "@/sanity/env";
+import { schema } from "@/sanity/schema";
+import { defaultDocumentNode } from "@/sanity/structure";
+import { theme } from "@/sanity/theme";
 
 export default defineConfig({
   basePath: "/studio",
