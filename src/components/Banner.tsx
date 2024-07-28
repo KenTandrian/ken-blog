8d402a1589a0c6e8c898088d485d1f935d9ee8ed
Ken Tandrian <60643640+KenTandrian@users.noreply.github.com>
2024-07-28 18:24:08 +0000

    feat(lint): configure eslint

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-
-const Banner = () => {
+export default function Banner() {
   return (
     <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
       <div>
-        <h1 className="text-7xl">Ken's Daily Blog</h1>
+        <h1 className="text-7xl">Ken&apos;s Daily Blog</h1>
         <h2 className="mt-5 md:mt-2">
           Welcome to{" "}
           <span className="underline decoration-4 decoration-[#F7AB0A]">
-            Every Developer's
+            Every Developer&apos;s
           </span>{" "}
           favourite blog in the Devosphere!
         </h2>
@@ -20,5 +18,3 @@ const Banner = () => {
     </div>
   );
 };
-
-export default Banner;
