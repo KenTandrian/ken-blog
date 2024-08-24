4abcf883824c4f04cbac6acc18b2bf6d41a00f12
KenTandrian <kenricktan11@gmail.com>
2024-08-24 15:25:59 +0700

    feat(dev): enable idx

---
+++ .idx/dev.nix
@@ -0,0 +1,29 @@
+{ pkgs }: {
+  channel = "stable-24.05";
+  packages = [
+    pkgs.nodejs_20
+    pkgs.yarn
+  ];
+  idx = {
+    extensions = [
+      "bradlc.vscode-tailwindcss"
+    ];
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ "yarn" "dev" "--port" "$PORT" ];
+          manager = "web";
+        };
+      };
+    };
+    workspace = {
+      onCreate = {
+        create-env = "cp .env-example .env";
+      };
+      onStart = {
+        install-deps = "yarn install";
+      };
+    }
+  };
+}
