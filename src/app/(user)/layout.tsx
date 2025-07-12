3208da18f2d4341626dfce5b75e100354148482a
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2025-07-12 17:03:21 +0000

    fix(app): duplicate html and body layout

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -9,17 +9,15 @@ import "@/styles/globals.css";
 async function RootLayout({ children }: React.PropsWithChildren) {
   const { isEnabled: isDraftMode } = await draftMode();
   return (
-    <html>
-      <body>
-        {isDraftMode && <PreviewBanner />}
-        <div className="max-w-7xl mx-auto">
-          <Header />
-          <Banner />
-          {children}
-          {isDraftMode && <LiveVisualEditing />}
-        </div>
-      </body>
-    </html>
+    <>
+      {isDraftMode && <PreviewBanner />}
+      <div className="max-w-7xl mx-auto">
+        <Header />
+        <Banner />
+        {children}
+        {isDraftMode && <LiveVisualEditing />}
+      </div>
+    </>
   );
 }
 
