126a25ae5d134f36a4315c835580b323e5c7b73a
Kenrick Tandrian <60643640+KenTandrian@users.noreply.github.com>
2025-07-12 17:01:15 +0000

    feat(api): update draft API

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,23 +1,8 @@
-import { validatePreviewUrl } from "@sanity/preview-url-secret";
-import { draftMode } from "next/headers";
-import { redirect } from "next/navigation";
-import { NextResponse, type NextRequest } from "next/server";
+import { defineEnableDraftMode } from "next-sanity/draft-mode";
 
 import { client } from "@/sanity/lib/client";
 import { token } from "@/sanity/lib/token";
 
-const clientWithToken = client.withConfig({ token });
-
-export async function GET(request: NextRequest) {
-  const { isValid, redirectTo = "/" } = await validatePreviewUrl(
-    clientWithToken,
-    request.url
-  );
-
-  if (!isValid) {
-    return new NextResponse("Invalid secret", { status: 401 });
-  }
-
-  (await draftMode()).enable();
-  redirect(redirectTo);
-}
+export const { GET } = defineEnableDraftMode({
+  client: client.withConfig({ token }),
+});
